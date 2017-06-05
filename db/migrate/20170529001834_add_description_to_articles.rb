5fb465102fda2b5677ae43e2776ed1dadb0d4b07
watchain <watchain@yahoo.com>
2017-06-05 17:51:31 +0000

    delete migrate file

--- db/migrate/20170529001834_add_description_to_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddDescriptionToArticles < ActiveRecord::Migration
-  def change
-    add_column :article, :description, :text 
-    add_column :article, :created_at, :datetime
-    add_column :article, :updated_at, :datetime
-  end
-end
